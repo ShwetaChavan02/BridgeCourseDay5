21ebdf75d62b5df457017e4f1c00e72af7881d10
ShwetaChavan02 <shwetachavan0204@gmail.com>
2026-09-21 15:43:05 +0530

    palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,2 +1,17 @@
 # program to identify whether the number is a palindrome or not 
 # if yes return yes, if no return no
+
+num = int(input("Enter any number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original == reverse:
+    print("It is a palindrome")
+else:
+    print("It is not a palindrome")
