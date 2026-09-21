e2656889e60d3b275c2ad9c449e2f6539d8310c8
ShwetaChavan02 <shwetachavan0204@gmail.com>
2026-09-21 15:42:30 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,2 @@
+# program to identify whether the number is a palindrome or not 
+# if yes return yes, if no return no
--- reverse.py
+++ reverse.py
@@ -1,13 +1,16 @@
-
 # Program to reverse the given number
 
 num = int(input("Enter any number: "))
+
+original = num
 reverse = 0
 
-while num != 0:
+while num > 0:
     digit = num % 10
     reverse = reverse * 10 + digit
     num = num // 10
 
-print("Reverse of the given number is:", reverse)
-
+if original == reverse:
+    print("It is a palindrome")
+else:
+    print("It is not a palindrome")
